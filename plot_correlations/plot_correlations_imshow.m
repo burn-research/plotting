3671b95a64da014928661927e2be6dca11e2c49f
camillejr <kamilazdybal@gmail.com>
2020-05-26 15:40:18 +0200

    Added new function for plotting PCC

---
+++ plot_correlations/plot_correlations_imshow.m
@@ -0,0 +1,32 @@
+function [] = plot_correlations_imshow(correlations_matrix, annotations, k_list, destination)
+
+
+fontsize_axes = 16;
+fontsize_label = 24;
+fontsize_colorbar = 16;
+
+
+figure(1);
+set(gcf, 'Units', 'Normalized', 'OuterPosition', [0, 0, 1, 0.6])
+imagesc(correlations_matrix)
+set(gca, 'FontSize', fontsize_axes);
+xticks(1:1:length(annotations))
+xticklabels(annotations)
+xtickangle(90)
+yticks(1:1:length(k_list))
+yticklabels(k_list)
+ylabel('k [-]', 'FontSize', fontsize_label)
+% axis('equal')
+
+% colormap(parula);
+cb = colorbar;
+cb.FontSize = fontsize_colorbar;
+cb.FontName = 'cmr10';
+set(cb, 'ticks', [0:0.1:1], 'ticklabels', [0:0.1:1])
+% set(get(cb,'label'), 'string','$r$ [-]');
+
+% Save plot:
+filename = [destination, 'correlations_imshow.png'];
+saveas(gcf, filename, 'png');
+
+end
